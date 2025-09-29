ae3dfe38ea65272d7f96865c1a5fddde5e35065b
Eesha <eeshasingapuri@gmail.com>
2025-09-29 01:44:52 -0400

    Chapter 2

--- C#milestones/Assets/Scripts/LearningCurve.cs
+++ C#milestones/Assets/Scripts/LearningCurve.cs
@@ -5,10 +5,18 @@ public class LearningCurve : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Hello
         int myInt = 62;
         float myFloat = 7.76543f;
         string myString = "Hey";
         bool myBool = true;
+
+        /* Okay
+        bye */
+
+        /// <summary>
+        /// This is for chapter 2
+        /// </summary>
 
     }
 
