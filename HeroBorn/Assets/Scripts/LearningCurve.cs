40d5f56235f6effb7cda0f79cf536b4d2fbd2d98
Eesha <eeshasingapuri@gmail.com>
2025-09-29 02:21:24 -0400

    Update LearningCurve.cs (Ch2 variables & comments)

---
+++ HeroBorn/Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int myInt = 40;
+        float myFloat = 3.2f;
+        string myString = "Hi Unity";
+        bool myBool = true;
+
+        void Start()
+        {
+            //single line comment
+
+            /*multi-line 
+            comment*/
+
+            ///<summary>
+            /// summary for chapter 2
+            /// </summary>
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
