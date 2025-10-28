9ab21a98601b49e1d4744d789d116ca373dc2a9c
Eesha <eeshasingapuri@gmail.com>
2025-10-28 12:37:24 -0400

    Chapter 7: Camera Controls

---
+++ HeroBorn/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    void LateUpdate()
+    {
+        transform.position = player.position + offset;
+        transform.LookAt(player);
+    }
+}
