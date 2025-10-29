df2ba74f4cfefa10faf971ff0fa71e8e73a7c1a3
Eesha <eeshasingapuri@gmail.com>
2025-10-28 23:12:57 -0400

    Chapter 8: Character Control (jumping and groundcheck)

--- HeroBorn/Assets/Scripts/PlayerMovement.cs
+++ HeroBorn/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Xml;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -5,16 +6,48 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public float turnSpeed = 90f;
 
+    private Rigidbody rb;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
     void Update()
     {
-        // keyboard input (WSAD/Arrows)
         float move = Input.GetAxis("Vertical");
         float turn = Input.GetAxis("Horizontal");
 
-        // to move forward&backward
-        transform.Translate(Vector3.forward * move * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * move * moveSpeed * Time.deltaTime, Space.Self);
+        transform.Rotate(Vector3.up, turn * turnSpeed * Time.deltaTime, Space.Self);
 
-        // rotate
-        transform.Rotate(Vector3.up, turn * turnSpeed * Time.deltaTime);
+        if (groundCheck != null)
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        }
+        else
+        {
+            isGrounded = true;
+        }
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        Vector3 v = rb.linearVelocity;
+        if (v.y < 0f) v.y = 0f;
+        rb.linearVelocity = v;
+
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
